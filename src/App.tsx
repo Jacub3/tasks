ba042575dff0611d36d8b38eac0ed3fb6ead505a
Jacob Elliott <jello@udel.edu>
2025-09-07 20:22:55 -0400

    added boxes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./App.css";
-import { Button } from "react-bootstrap";
+import { Button, Container, Row, Col } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                <h1>Hello World</h1>
                 UD CISC275 with React Hooks and TypeScript - Jacob Elliott
             </header>
+            <h1>Hello World</h1>
             <img
                 src="C:\Users\ellio\OneDrive\Desktop\tasks\src\download.jpg"
                 alt="A picture of my favorite character"
@@ -18,7 +18,42 @@ function App(): React.JSX.Element {
                 <li>Shrek is life</li>
                 <li>A third thing</li>
             </ol>
-            <Button>Log Hello World</Button>
+            <Button
+                onClick={() => {
+                    console.log("I am logged");
+                }}
+            >
+                Log Hello World
+            </Button>
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                width: "160px",
+                                height: "100px",
+                                backgroundColor: "black",
+                                marginTop: "10px",
+                            }}
+                        />
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                width: "160px",
+                                height: "100px",
+                                backgroundColor: "black",
+                                marginTop: "10px",
+                            }}
+                        />
+                    </Col>
+                    <Col>
+                        Second column. You can put whatever you want in here,
+                        and it will be on the right side. Maybe try adding an
+                        image?
+                    </Col>
+                </Row>
+            </Container>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
