46e0ab1359135026eb2efcff68a24571a9b77401
Jacob Elliott <jello@udel.edu>
2025-09-17 22:21:33 -0400

    finished duplicateQuestion

--- src/objects.ts
+++ src/objects.ts
@@ -69,7 +69,7 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    return question.id + ": " + question.name.substring(0, 10);
 }
 
 /**
@@ -90,7 +90,19 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    let markdown = `# ${question.name}\n${question.body}`;
+
+    if (
+        question.type === "multiple_choice_question" &&
+        Array.isArray(question.options)
+    ) {
+        const optionsMarkdown = question.options
+            .map((option) => `- ${option}`)
+            .join("\n");
+        markdown += `\n${optionsMarkdown}`;
+    }
+
+    return markdown;
 }
 
 /**
@@ -98,7 +110,7 @@ export function toMarkdown(question: Question): string {
  * `newName`.
  */
 export function renameQuestion(question: Question, newName: string): Question {
-    return question;
+    return { ...question, name: newName };
 }
 
 /**
@@ -107,7 +119,10 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    return {
+        ...question,
+        published: !question.published,
+    };
 }
 
 /**
@@ -117,7 +132,16 @@ export function publishQuestion(question: Question): Question {
  * The `published` field should be reset to false.
  */
 export function duplicateQuestion(id: number, oldQuestion: Question): Question {
-    return oldQuestion;
+    const newQuestion = {
+        ...oldQuestion,
+        name: "Copy of " + oldQuestion.name,
+        body: oldQuestion.body,
+        type: oldQuestion.type,
+        id: id,
+        published: false,
+        options: [...oldQuestion.options],
+    };
+    return newQuestion;
 }
 
 /**
